44b5db8ad60a37c664d324c3f94c8fddb8dee5f2
JoseLuis-Figueroa <figueroadelara@outlook.com>
2025-08-19 15:44:22 -0600

    Create project firmware code.
    Create a MCUXpresso project to implement the driver.

---
+++ Firmware-Code/GPIO-Driver/source/main.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2016-2025 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * @file    GPIO-Driver.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+/* TODO: insert other include files here. */
+
+/* TODO: insert other definitions and declarations here. */
+
+/*
+ * @brief   Application entry point.
+ */
+int main(void)
+{
+
+
+    return 0 ;
+}
