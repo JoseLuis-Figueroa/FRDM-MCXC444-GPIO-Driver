0dd15faff23d913a6763b7ce2b96f333fa79d4c2
JoseLuis-Figueroa <figueroadelara@outlook.com>
2025-08-21 15:24:23 -0600

    Create gpio_cfg.h.
    Create and add a basic template for the gpio configuration header.

---
+++ Firmware-Code/GPIO-Driver/drivers/configuration/gpio_cfg.h
@@ -0,0 +1,35 @@
+/**
+ * @file gpio_cfg.h
+ * @author Jose Luis Figueroa
+ * @brief This module contains interface definitions for the GPIO driver
+ * configuration. This is the header file for the definition of the
+ * interface for retrieving the general porpuse input/output configuration
+ * table.
+ * @version 0.0
+ * @date 2025-21-08
+ * 
+ * @copyright Copyright (c) 2025 Jose Luis Figueroa. MIT License.
+ * 
+ */
+
+#ifndef GPIO_CFG_H
+#define GPIO_CFG_H
+
+/*****************************************************************************
+* Includes
+*****************************************************************************/
+#include <stdio.h>
+
+/*****************************************************************************
+* Preprocessor Constants
+*****************************************************************************/
+
+/*****************************************************************************
+* Typedefs
+*****************************************************************************/
+
+/*****************************************************************************
+* Function Prototypes
+*****************************************************************************/
+
+#endif /* GPIO_CFG_H */
