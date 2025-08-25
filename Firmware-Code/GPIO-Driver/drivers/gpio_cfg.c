571b086fead6e11a770eeb0ff21b58640c1f7599
JoseLuis-Figueroa <figueroadelara@outlook.com>
2025-08-25 09:40:38 -0600

    Create and fill the configuration table.
    Fill the configuration table with the configuration data for each general-purpose input/output pin.

--- Firmware-Code/GPIO-Driver/drivers/gpio_cfg.c
+++ Firmware-Code/GPIO-Driver/drivers/gpio_cfg.c
@@ -32,8 +32,20 @@
 /*****************************************************************************
 * Module Variable Definitions
 *****************************************************************************/
-
-
+/**
+ * The following array contains the configuration data for each general-purpose
+ * input/output peripheral channel (pin). Each row represent a single pin.
+ * Each column is representing a member of the GPIO_Config_t structure. This 
+ * table is read in by GPIO_Init, where each channel is then set up based on 
+ * this table.
+*/
+const GPIO_Config_t gpioConfigTable[] = 
+{
+    /* {Port, Pin, Function, Slew Rate, Resistor} */
+    {GPIO_PTD, GPIO_PTD5,  GPIO_AF1, GPIO_SLOW_SLEW, GPIO_NO_RESISTOR}, // GREEN LED
+    {GPIO_PTE, GPIO_PTE29, GPIO_AF1, GPIO_SLOW_SLEW, GPIO_NO_RESISTOR}, // BLUE LED
+    {GPIO_PTE, GPIO_PTE31, GPIO_AF1, GPIO_SLOW_SLEW, GPIO_NO_RESISTOR}  // RED LED
+};
 
 /*****************************************************************************
 * Function Prototypes
