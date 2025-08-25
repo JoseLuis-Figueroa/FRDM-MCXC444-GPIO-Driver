ce845263310f49e0808c3bf2cd227ab2923f9b66
JoseLuis-Figueroa <figueroadelara@outlook.com>
2025-08-25 10:41:40 -0600

    Add the  GPIO_GetConfigTable function.
    The GPIO_GetConfigTable is used to initialize the GPIO based on the configuration table.

--- Firmware-Code/GPIO-Driver/drivers/gpio_cfg.c
+++ Firmware-Code/GPIO-Driver/drivers/gpio_cfg.c
@@ -39,7 +39,7 @@
  * table is read in by GPIO_Init, where each channel is then set up based on 
  * this table.
 */
-const GPIO_Config_t gpioConfigTable[] = 
+const GPIO_Config_t configTable[] = 
 {
     /* {Port, Pin, Function, Slew Rate, Resistor} */
     {GPIO_PTD, GPIO_PTD5,  GPIO_AF1, GPIO_SLOW_SLEW, GPIO_NO_RESISTOR}, // GREEN LED
@@ -54,3 +54,46 @@ const GPIO_Config_t gpioConfigTable[] =
 /*****************************************************************************
 * Function Definitions
 *****************************************************************************/
+/*****************************************************************************
+ * Function: GPIO_GetConfigTable()
+*/
+/**
+*\b Description:
+ * This function returns a pointer to the configuration table defined in this 
+ * module. The configuration table is used by the GPIO driver to initialize 
+ * the GPIO peripheral.
+ * 
+ * PRE-CONDITION: The GPIO port clock must be enabled in the SIM module. <br> 
+ * PRE-CONDITION: The configuration table needs to be populated (sizeof > 0) <br>
+ * 
+ * @return A constant pointer to the first member of the configuration table
+ * will be returned. <br>
+ *  
+ * \b Example: 
+ * @code
+ * const GPIO_Config_t * const GPIO_Config = GPIO_GetConfigTable();
+ * size_t GPIO_configSize = GPIO_GetConfigTableSize();
+ * 
+ * GPIO_Init(GPIO_Config, GPIO_configSize);
+ * @endcode
+ * 
+ * @see GPIO_GetConfigTable
+ * @see GPIO_GetConfigTableSize
+ * @see GPIO_init
+ * @see GPIO_channelRead
+ * @see GPIO_channelWrite
+ * @see GPIO_channelToggle
+ * @see GPIO_registerWrite
+ * @see GPIO_registerRead
+ * 
+ * 
+*****************************************************************************/
+const GPIO_Config_t * const GPIO_GetConfigTable(void)
+{
+   /* The cast is performed to ensure that the address of the first element 
+    * of configuration table is returned as a constant pointer and not a
+    * pointer that can be modified
+   */
+  return (const GPIO_Config_t*)&configTable[0];
+
+}
